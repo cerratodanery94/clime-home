3df55421c99c8325dcbec0b6842f6f9cf757b427
cerratodanery <cerratodanery94@gmail.com>
2020-02-07 13:25:20 -0600

    cuarto comit

--- vistas/registro_vista.php
+++ vistas/registro_vista.php
@@ -28,11 +28,11 @@
 
     <form action="login_modelo.php" method="post">
       <div class="form-group has-feedback">
-        <input type="text" class="form-control nombres" placeholder="nombres" name="nombres" id="nombres">
+        <input type="text" class="form-control nombres" placeholder="Nombres" name="nombres" id="nombres">
         <span class="glyphicon glyphicon-user form-control-feedback"></span>
       </div>
       <div class="form-group has-feedback">
-        <input type="text" class="form-control apellidos" placeholder="apellidos" name="apellidos" id="apellidos">
+        <input type="text" class="form-control apellidos" placeholder="Apellidos" name="apellidos" id="apellidos">
         <span class="glyphicon glyphicon-user form-control-feedback"></span>
       </div>
 
@@ -41,19 +41,15 @@
         <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
       </div>
       <div class="form-group has-feedback">
-        <input type="password" class="form-control contraseña" placeholder="contraseña" name="contraseña" id="contraseña">
+        <input type="password" class="form-control contraseña" placeholder="Contraseña" name="contraseña" id="contraseña">
         <span class="glyphicon glyphicon-lock form-control-feedback"></span>
       </div>
       <div class="form-group has-feedback">
-        <input type="email" class="form-control correo" placeholder="correo" name="correo" id="correo">
+        <input type="email" class="form-control correo" placeholder="correo" name="Correo" id="correo">
         <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
       </div>
       <div class="row">
-        <div class="col-xs-8">
-          <div class="checkbox icheck">
-           
-          </div>
-        </div>
+        
         <!-- /.col -->
         <div class="col-xs-4">
           <button type="submit" class="btn btn-primary btn-registar btn-block btn-flat">Ingresar</button>
