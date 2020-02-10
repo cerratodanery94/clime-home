b3d38a8fc54fb6b541b734fed93bbfefc0b2c914
abnerflores04 <60516827+abnerflores04@users.noreply.github.com>
2020-02-10 10:31:10 -0600

    Update registrar_modelo.php

--- modelos/registrar_modelo.php
+++ modelos/registrar_modelo.php
@@ -6,7 +6,7 @@ $usuario= strtoupper($_POST["usuario"]);
 $contra= $_POST["contra"];
 $pass_cifrado=password_hash($contra,PASSWORD_DEFAULT,array("cost"=>12));
 $estado="NUEVO";
-$correo= $_POST["correo"];	
+$correo= $_POST["correo"];
 	try{
 		require '../modelos/conectar.php';
 
@@ -19,8 +19,8 @@ $correo= $_POST["correo"];
 		    
        }else{	
 		   
-	   $sql="INSERT INTO USUARIO (ROL_CODIGO,USU_USUARIO,USU_NOMBRES,USU_APELLIDOS,USU_PASSWORD,USU_ESTADO,USU_CORREO) 
-	   VALUES (:rol,:usuario,:nombres,:apellidos,:contra,:estado,:correo)";
+	   $sql="INSERT INTO USUARIO (ROL_CODIGO,USU_USUARIO,USU_NOMBRES,USU_APELLIDOS,USU_PASSWORD,USU_ESTADO,USU_FECHA_ULTIMA_CONEXION,USU_PREGUNTAS_CONTESTADAS,USU_PRIMER_INGRESO,USU_FECHA_VENCIMIENTO,USU_CORREO) 
+	   VALUES (:rol,:usuario,:nombres,:apellidos,:contra,:estado,'','','','',:correo)";
 	
 	   $resultado=$conexion->prepare($sql);	
        $resultado->execute(array(":rol"=>$idrol,":usuario"=>$usuario,":nombres"=>$nombres,":apellidos"=>$apellidos,":contra"=>$pass_cifrado,":estado"=>$estado,":correo"=>$correo));
