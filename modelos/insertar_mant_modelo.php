ae2fd182e00f205a316ec8fdca214add364c0008
cerratodanery <cerratodanery94@gmail.com>
2020-02-22 22:07:09 -0600

    comit correo

--- modelos/insertar_mant_modelo.php
+++ modelos/insertar_mant_modelo.php
@@ -30,6 +30,18 @@ $fecha_vencimiento= date("Y-m-d H:m:s",strtotime("+1 years"));
 $correo= $_POST["correo"];
 
 	try{
+	
+      
+
+		require '../modelos/conectar.php';
+		$consulta=$conexion->prepare("SELECT * FROM TBL_USUARIO WHERE USU_USUARIO='$usuario' or USU_CORREO='$correo'");
+        $consulta->execute();
+        $num_rows = $consulta->fetchColumn();
+        
+       if ($num_rows>0){ 
+		   echo '<script>alert("Usuario o Correo ya se encuentran registrados ");window.location= "../vistas/insertar_mant_vista.php"</script>';
+		    
+       }else{	
 		$template_correo=file_get_contents('../vistas/template_correo.php');
 		$template_correo=str_replace("{{pass}}",$pass,$template_correo);
 		$template_correo=str_replace("{{year}}",date('Y'),$template_correo);
@@ -53,17 +65,6 @@ $correo= $_POST["correo"];
 		$mail->Body    = $template_correo;
 
 		$mail->send();
-      
-
-		require '../modelos/conectar.php';
-		$consulta=$conexion->prepare("SELECT * FROM TBL_USUARIO WHERE USU_USUARIO='$usuario' or USU_CORREO='$correo'");
-        $consulta->execute();
-        $num_rows = $consulta->fetchColumn();
-        
-       if ($num_rows>0){ 
-		   echo '<script>alert("Usuario o Correo ya se encuentran registrados ");window.location= "../vistas/insertar_mant_vista.php"</script>';
-		    
-       }else{	
 		   
 	   $sql="INSERT INTO TBL_USUARIO (ROL_CODIGO,USU_USUARIO,USU_NOMBRES,USU_APELLIDOS,USU_PASSWORD,USU_ESTADO,USU_PREGUNTAS_CONTESTADAS,USU_PRIMER_INGRESO,USU_FECHA_CREACION,USU_FECHA_VENCIMIENTO,USU_TOKEN,USU_FECHA_TOKEN,USU_CORREO) 
 	   VALUES (:rol,:usuario,:nombres,:apellidos,:contra,:estado,'','',:fecha_creacion,:fecha_vencimiento,'','',:correo)";
